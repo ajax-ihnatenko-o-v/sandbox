6dcaf2b31b77d4c0e838eadc33e98cab2d1c20b3
Ihnatenko Oleksandr <ihnatenko.o.v@ajax.systems>
2023-09-20 18:20:23 +0300

    add groovy script

---
+++ test_pipeline.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent { label 'ci-x86-enc-u18.04-desktop-slave' }
+
+    options {
+		disableConcurrentBuilds()
+        timeout(time: 20)
+        timestamps()
+    }
+
+    stages {
+    // ----  Checkout ----
+    stage('Checkout') {
+        steps {
+            script{
+                def scmInfo = checkout scm
+                echo "scm: ${scmInfo}"
+            }
+        }
+    }
+
+    // -------------- Create a virtualenv --------------
+    stage('Sleep') {
+        steps{
+            script{
+                echo "Before sleep"
+                sleep(30)
+                echo "After sleep"
+            }
+        }
+    }
+}
